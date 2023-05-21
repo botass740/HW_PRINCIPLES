89a6034463ec4fc3d9f3b342a37ca62d15f72e47
Max test <botass740@gmail.com>
2023-05-21 23:09:16 +0300

    Initial commit

---
+++ src/main/java/ru/netology/FormDate.java
@@ -0,0 +1,9 @@
+package ru.netology;
+
+import com.sun.jdi.request.StepRequest;
+
+public class FormDate {
+        public int day;
+        public int month;
+        public int year;
+}
---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,15 @@
+package ru.netology;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+
+    //FormDate birthday = new FormDate();
+
+
+}
